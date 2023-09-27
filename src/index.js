a277a1d9afe65ec0d883921f5f9069187a3cebb3
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2023-09-28 00:48:20 +0200

    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-console.log('test');
+//console.log('test');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
